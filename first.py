e946e9f6756ee9b0417e2de78b8a6e3ffb57d095
Melon Deau <melondeau@MacBook-Air-de-Melon.local>
2023-04-26 15:41:44 +0200

    second commit

--- first.py
+++ first.py
@@ -1,11 +1,13 @@
 import time 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome('./chromedriver')
 
 driver.get('https://www.oanda.com/currency-converter/fr/?from=EUR&to=USD&amount=NaN')
-time.sleep(3)
+time.sleep(5)
 
 champs_euro = driver.find_element(By.CLASS_NAME, "numberformat").send_keys('34')
 
