3a4cd4ef7bf81c68f72b168c67391063dcbea371
Melon Deau <melondeau@MacBook-Air-de-Melon.local>
2023-04-28 15:31:33 +0200

    fiche_prod

---
+++ first_fiche.py
@@ -0,0 +1,50 @@
+import time 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import pandas as pd 
+from openpyxl.workbook import Workbook
+
+EXPORT_PATH = "./exports/"
+
+url = "https://eberhardt-pro.fr/armoires-refrigerees/5731-congelateur-electromenager-no-frost-6-tiroirs-238l.html"
+
+s=Service('./chromedriver')
+driver = webdriver.Chrome(service=s)
+#driver.add_argument("headless") #to hide the open-load 
+
+
+driver.get(url)
+time.sleep(2)
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+def clean_text(str):
+    return str.replace('\n', " ").replace('\t', " ")
+#---------- Recupération du titre et de la ref 
+for title in soup.find('h1',class_= "h1") : 
+      print(title.get_text(strip=True))
+for ref in soup.find('p',class_= "d-none") : 
+     print(ref.get_text(strip=True))
+
+
+#---------- Tab et Description du produit 
+items = soup.find_all("div", {"class": "col-12 col-sm-6 product--description--content--item"})
+#print(items)
+
+item = items[0]
+#type = clean_text(item.find('span').get_text(strip=True))
+#print(type)
+
+caract = []
+for item in items : 
+    type = clean_text(item.find('span').get_text(strip=True))
+    print(type)
+
+
+#produits = {
+#        "title" : title,
+#        'ref':  ref,
+#    }
+#print(caract.append(produits))
