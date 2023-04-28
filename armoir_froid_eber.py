40508ff973916fe582d1609aff8bf585d2fa2a87
Melon Deau <melondeau@MacBook-Air-de-Melon.local>
2023-04-28 11:46:48 +0200

    print list produits

--- armoir_froid_eber.py
+++ armoir_froid_eber.py
@@ -1,10 +1,10 @@
 import time 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-#from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-nb_armoir = 150
+nb_armoir_looking = 120
 
 #------- load and sleep the scrap page 
 url = "https://eberhardt-pro.fr/cuisine-professionnelle/29-armoires-refrigerees?page=2"
@@ -20,13 +20,48 @@ soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 
 #------- find the H2 on the page 
-for h2 in soup.findAll('h2') :
-    nom_du_produit = h2.text.strip()
+#for h2 in soup.findAll('h2',class_= "h6 product-title mt-2") : 
+#   print(h2.get_text(strip=True))
 
-#print(nom_du_produit)
+#------- Load other pages (next pages) and take the others H2
+
+#nb_click = nb_armoir_looking / 30 - 1 if h2 >= int(nb_armoir_looking) else h2 / 30 #pour linstant ça c'est faut car on recupere pas un numéro
+#nb_click
+#for i in range(0, int(nb_click)): 
+#    button_next30 = driver.find_element(By.CLASS_NAME('material-icons'))
+   # button_next30.click() -> js to execute the click 
+#    driver.execute_script('arguments[0]', button_next30)
+#    time.sleep(2)
+ 
+#soup = BeautifulSoup(driver.page_source, 'html.parser') #MAJ of the new code 
+#driver.quit()
+ #------- retrieve  one item
+def clean_text(str):
+    return str.replace('\n', " ").replace('\t', " ")
 
 
-#------- Load other pages (next pages)
+items = soup.find_all("div", {"class": "thumbnail-container"})
+#print(len(items))
+
+item = items[0]
+code_art = clean_text(item.find("div", {'class': 'product-reference'}).get_text(strip=True))
+#print(code_art)
+
+#-------  retrieve items and sotck it in a list 
+
+list_produits = []
+
+for item in items : 
+
+    title = clean_text(item.find('h2',class_= "h6 product-title mt-2").get_text(strip=True))
+    code_art = clean_text(item.find("div", {'class': 'product-reference'}).get_text(strip=True))
+    
+    produit = {
+        "title" : title,
+        'code_art':  code_art
+    }
+    list_produits.append(produit)
+print(list_produits)
 #------- Send to CSV
 
 
